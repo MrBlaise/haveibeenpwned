c1ea615891cf6f2a9f0df42125245a3801769afc
Balázs Rostás <rostas.balazs@gmail.com>
2018-04-23 08:38:57 +0200

    First version released

---
+++ haveibeenpass.sh
@@ -0,0 +1,85 @@
+#!/bin/bash -aeu
+
+set -o pipefail
+
+function usage_exit {
+    echo "${0} {password} [-f -d -i -p]"
+    echo "Options:"
+    echo -e "\t-f --file         to use a file of passwords (new line seperated)"
+    echo -e "\t-d --delay        to set delay (defaults to 2s to prevent rate limit)"
+    echo -e "\t-i --interactive  start interactive mode"
+    echo -e "\t-p --plain        print plain passwords and their statuses" 
+    echo "Example: ${0} 12345"    
+    echo "Example: ${0} --file=password_file.txt --delay=2 --plain"    
+    echo "Example: ${0} --interactive"    
+    echo "Example: ${0} --interactive --plain"    
+    exit 1
+}
+
+function check_password {
+    (curl -s https://api.pwnedpasswords.com/range/$(echo -n "${1}" | shasum | cut -b 1-5) | grep $(echo -n "${1}" | shasum | cut -b 6-40 | tr a-f A-F))
+}
+
+function check_password_interactive {
+    echo -n "Password:"
+    read -s pw
+    echo -ne " Loading...\033[0K\r"
+    if [[ ! -z "${PLAIN:-}" ]]; then
+      check_password_with_plain_results ${pw}
+    else
+      check_password "${pw}"
+    fi
+    exit $?
+}
+
+function check_password_with_plain_results {
+    echo -n "${1}" | (check_password "${1}" | xargs -0 -I {} echo -n "FOUND: ${1} -- {}") || echo "SAFE: ${1}" 
+}
+
+test "$#" -gt 0 || usage_exit
+
+for i in "$@"
+do
+case $i in
+    -f=*|--file=*)
+    FILE="${i#*=}"
+    shift 
+    ;;
+    -d=*|--delay=*)
+    DELAY="${i#*=}"
+    shift 
+    ;;
+    -i|--interactive)
+    INTERACTIVE=1	    
+    shift 
+    ;;
+    -p|--plain)
+    PLAIN=1	    
+    shift 
+    ;;
+    *)
+    check_password "${1}"
+    exit $?
+    ;;
+esac
+done
+
+if [[ ! -z "${INTERACTIVE:-}" ]]; then
+    check_password_interactive
+fi
+
+if [[ -z "${DELAY:-}" ]]; then
+    DELAY=2
+fi
+
+if [[ ! -z "${FILE:-}" ]]; then
+    while read -r line; do
+      if [[ ! -z "${PLAIN:-}" ]]; then
+	  echo -n "${line}" | check_password_with_plain_results "${line}"
+      else
+          echo -n "${line}" | check_password "${line}" || :
+      fi
+      sleep "${DELAY}"
+    done < "${FILE}"    
+fi
+
